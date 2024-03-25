14f0cad8cf9837d9c7bc4f16c455870dce3aa220
Andre Herms <aherms@aherms.de>
2024-03-25 19:57:37 +0100

    wip

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+
+
 rooms = {}
 rooms.Garten = {
     "img": "https://media01.living4media.com/largepreviews/NDI0NjY5NzQ0/13699024-Kleingarten-im-Herbst-mit-Apfelbaum-und-Glashaus.jpg",
@@ -42,10 +44,50 @@ rooms.Teich = {
     }
 }
 
-player = {}
+player = {
+    "actions": ["nimm", "betrachte"],
+}
 player.room = "Garten"
 player.inventory = ["Taschenmesser"]
 player.game =  {
     "rooms": rooms
+}
+
+
+// program code below
+
+function select_item(item) {
+
+}
+
+function select_action(action) {
+    player.game.current_action = action;
+}
+
+function move_to(direction) {
+
+    let new_room = player.game.rooms[direction];
+    
+    const direction_list = document.getElementById("dir-list");
+    const new_list = document.createElement("span");
+    
+    for (const [dir, room]  of Object.entries(new_room.ways)) {
+        const b =document.createElement('button');
+        b.appendChild(document.createTextNode(dir));
+        b.onclick =  () => move_to(room);
+        new_list.appendChild(b);
+    }
+
+    direction_list.replaceWith(new_list);
+    new_list.setAttribute("id", "dir-list");
+
+    const bild = document.getElementById("bild");
+    bild.setAttribute("src", new_room.img);
+
+}
+
+
+function start() {
+    move_to("Garten");
 }
 
