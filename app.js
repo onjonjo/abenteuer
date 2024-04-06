5629a3acc46dc048715230edbd4953058195e603
Andre Herms <aherms@aherms.de>
2024-04-06 12:27:18 +0200

    better text

--- app.js
+++ app.js
@@ -107,7 +107,8 @@ answers = {
 
 player = {
 }
-player.log = ""
+player.log = "Willkommen im unserem Spiel. Du kannst die Richtung mit den Buttons wählen. Du kannst auch Dinge betrachten, " + 
+      "nehmen und benutzen. Deine Ziel ist, dass die Katze dein Freund wird.";
 player.room = "Garten"
 player.commands = ["Betrachte", "Nimm", "Benutze"]
 player.current_command = null;
@@ -291,7 +292,7 @@ function build_room(new_room_name) {
 
 
 function start() {
-    add_to_log("Willkommen im unserem Spiel. Du kannst die Richtung mit den Buttons wählen. Du kannst auch Dinge betrachten, nehmen und benutzen. Viel Spaß!");
+    add_to_log("Los gehts!");
     build_room("Garten");
 }
 
